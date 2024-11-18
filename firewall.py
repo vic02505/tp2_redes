8ed81780a6a84e4ccfc67e5389125b486829f0f6
MateoLardiez <mlardiez@fi.uba.ar>
2024-11-18 16:00:50 -0300

    starting with firewall and changing topology.py

---
+++ firewall.py
@@ -0,0 +1,29 @@
+#Coursera :
+#- Software Defined Networking ( SDN ) course
+#-- Programming Assignment : Layer -2 Firewall Application Professor : Nick Feamster
+#Teaching Assistant : Arpit Gupta
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
+from collections import namedtuple
+import os
+# Add your imports here ...
+log = core.getLogger()
+
+# Add your global variables here ...
+class Firewall(EventMixin):
+    def __init__(self):
+        self.listenTo(core.openflow)
+        log.debug("Enabling␣Firewall␣Module")
+    def _handle_ConnectionUp(self, event):
+        # Add your logic here ...
+        # TODO: if port == 80: descartar
+        # TODO: if host == 1 and dstPort == 5001 and protocolo UDP: descartar
+        return            
+
+    def launch():
+        # Starting the Firewall module
+        core.registerNew(Firewall)
--- topology.py
+++ topology.py
@@ -5,6 +5,7 @@ MINIMUM_SWITCHES = 1
 HOST = "h"
 SWITCH = "s"
 BASE_IP = "10.0.0."
+HOSTS_AMOUNT = 4
 
 class ChainedSwitchesTopology(Topo):
     def __init__(self, ns):
@@ -15,28 +16,30 @@ class ChainedSwitchesTopology(Topo):
         if  switches_amount < MINIMUM_SWITCHES:
             raise Exception("Error - El número de switches debe ser mayor o igual a 1.")
 
-        #Hosts building
-        hosts_list = []
-
-        host_amount =  switches_amount * 2
-        for i in range (0, host_amount):
-            hosts_list.append(self.addHost(name=HOST+str(i+1), ip=BASE_IP+str(i+1)))
-
         #Switches building
         switches_list = []
-
+        
         for i in range (0, switches_amount):
             switches_list.append(self.addSwitch(name=SWITCH+str(i+1)))
 
         for i in range (0, switches_amount - 1):
             self.addLink(switches_list[i], switches_list[i+1])
 
-        #Linking hosts with switches:
-        host_id = 0
-        for switch in switches_list:
-            self.addLink(hosts_list[host_id], switch)
-            host_id += 1
-            self.addLink(hosts_list[host_id], switch)
-            host_id += 1
 
+        #Hosts building
+        hosts_list = []
+
+        for i in range(0, HOSTS_AMOUNT):
+            hosts_list.append(self.addHost(name=HOST+str(i+1), ip=BASE_IP+str(i+1)))
+      
+
+        #Linking hosts to swtiches      
+        first_switch = switches_list[0]
+        self.addLink(hosts_list[0], first_switch)
+        self.addLink(hosts_list[1], first_switch)
+        
+        last_switch = switches_list[-1]
+        self.addLink(hosts_list[2], last_switch)        
+        self.addLink(hosts_list[3], last_switch)
+            
 topos = {'tp2': ChainedSwitchesTopology}
