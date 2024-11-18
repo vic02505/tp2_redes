35f63c54911b7fb7b7df528b8e948ce94a06d740
vic02505 <vzacarias@fi.uba.ar>
2024-11-18 12:58:13 -0300

    Fixe topology.py execution error

--- topology.py
+++ topology.py
@@ -7,10 +7,10 @@ SWITCH = "s"
 BASE_IP = "10.0.0."
 
 class ChainedSwitchesTopology(Topo):
-    def build(self, ns):
+    def __init__(self, ns):
         Topo.__init__(self)
 
-        switches_amount = ns
+        switches_amount = int(ns)
 
         if  switches_amount < MINIMUM_SWITCHES:
             raise Exception("Error - El número de switches debe ser mayor o igual a 1.")
@@ -20,13 +20,16 @@ class ChainedSwitchesTopology(Topo):
 
         host_amount =  switches_amount * 2
         for i in range (0, host_amount):
-            hosts_list.append(self.addHost(name=HOST+str(i), ip=BASE_IP+str(i)))
+            hosts_list.append(self.addHost(name=HOST+str(i+1), ip=BASE_IP+str(i+1)))
 
         #Switches building
         switches_list = []
 
         for i in range (0, switches_amount):
-            switches_list.append(self.addSwitch(name=SWITCH+str(i)))
+            switches_list.append(self.addSwitch(name=SWITCH+str(i+1)))
+
+        for i in range (0, switches_amount - 1):
+            self.addLink(switches_list[i], switches_list[i+1])
 
         #Linking hosts with switches:
         host_id = 0
@@ -35,6 +38,5 @@ class ChainedSwitchesTopology(Topo):
             host_id += 1
             self.addLink(hosts_list[host_id], switch)
             host_id += 1
-
 
 topos = {'tp2': ChainedSwitchesTopology}
