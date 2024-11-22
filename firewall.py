077caaf7474a168917ac0784bd67e5904037d5ad
TomasGalian <tomiezeg@gmail.com>
2024-11-21 21:22:58 -0300

    Add first implementations of rules

--- firewall.py
+++ firewall.py
@@ -7,9 +7,12 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
+from pox.lib.addresses import IPAddr
 from pox.lib.addresses import EthAddr
 from collections import namedtuple
 import os
+import pox.lib.packet as pkt
+
 # Add your imports here ...
 log = core.getLogger()
 
@@ -18,11 +21,39 @@ class Firewall(EventMixin):
     def __init__(self):
         self.listenTo(core.openflow)
         log.debug("Enabling␣Firewall␣Module")
+        
     def _handle_ConnectionUp(self, event):
-        # Add your logic here ...
-        # TODO: if port == 80: descartar
-        # TODO: if host == 1 and dstPort == 5001 and protocolo UDP: descartar
+        log.info("ConnectionUp for switch {}: ".format(event.dpid))
+        # Si no ponemos ningun if, se instalarán las reglas en todos los switches
+        if event.dpid == 1:                                 # Se conecta al primer switch nada mas
+            log.info("Seteando reaglas")
+            self.setRule_1(event)
+            self.setRule_2(event)
+            self.setRule_3(event)
         return            
+
+    def setRule_1(self, event):
+        rule = of.ofp_flow_mod()
+        rule.match.tp_dst = 80                              # Puerto destino 80
+        rule.match.dl_type = pkt.ethernet.IP_TYPE           # Tipo IP (Es obligatorio?)
+        # rule.match.nw_proto = 6                           # Protocolo TCP (Es obligatorio?)
+        event.connection.send(rule)
+
+    def setRule_2(self, event):
+        rule = of.ofp_flow_mod()
+        rule.match.dl_type = pkt.ethernet.IP_TYPE           # Tipo de paquete: IP
+        rule.match.nw_proto = pkt.ipv4.UDP_PROTOCOL         # Protocolo UDP (17 es el valor en decimal para UDP)
+        rule.match.nw_src = IPAddr("10.0.0.1")              # Dirección IP del host 1
+        rule.match.tp_dst = 5001                            # Puerto destino 5001
+        event.connection.send(rule)
+
+    def setRule_3(self, event, h1, h2):
+        rule = of.ofp_flow_mod()
+        rule.match.dl_type = pkt.ethernet.IP_TYPE           # Tipo de paquete: IP
+        rule.match.nw_src = IPAddr(h1)                      # Dirección IP del host 1
+        rule.match.nw_dst = IPAddr(h2)                      # Dirección IP del host 1
+        event.connection.send(rule)
+        
 
 def launch():
      # Starting the Firewall module
